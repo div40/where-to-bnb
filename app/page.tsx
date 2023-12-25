f3ee81aca77b34002ca5ead2065298217f2a1577
div40 <vidh40@gmail.com>
2023-12-25 17:43:21 +0530

    empty state for "no search results found"

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,17 @@
 import Image from "next/image";
+import Container from "./components/Container";
+import EmptyState from "./components/EmptyState";
 
 export default function Home() {
-  return <div className="text-3xl">WhereToBnB</div>;
+  const isEmpty = true;
+  if (isEmpty) {
+    return <EmptyState showReset />;
+  }
+  return (
+    <Container>
+      <div className="pt-24 grid grid-col-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+        <div>Future Listings</div>
+      </div>
+    </Container>
+  );
 }
