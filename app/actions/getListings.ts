7134b1d36d29132b689e5f30f95c506de8c6de3d
div40 <vidh40@gmail.com>
2023-12-26 19:32:46 +0530

    serach filter modal and logic

--- app/actions/getListings.ts
+++ app/actions/getListings.ts
@@ -2,6 +2,13 @@ import prisma from "@/app/libs/prismadb";
 
 export interface IListingsParams {
     userId?: string;
+    guestCount?: number;
+    roomCount?: number;
+    bathroomCount?: number;
+    startDate?: string;
+    endDate?: string;
+    locationValue?: string;
+    category?: string;
 }
 
 export default async function getListings(
@@ -9,15 +16,67 @@ export default async function getListings(
 ){
     try {
 
-        const { userId } = params;
+        const { 
+            userId,
+            guestCount,
+            roomCount,
+            bathroomCount,
+            startDate,
+            endDate,
+            locationValue,
+            category
+         } = params;
 
         let query: any = {};
 
         if (userId){
-
             query.userId = userId;
-
         }
+
+        if (category){
+            query.category = category;
+        }
+        if (userId){
+            query.userId = userId;
+        }
+        if (guestCount){
+            query.guestCount = {
+                gte: +guestCount
+            };
+        }
+        if (roomCount){
+            query.roomCount = {
+                gte: +roomCount
+            };
+        }
+        if (bathroomCount){
+            query.bathroomCount = {
+                gte: +bathroomCount
+            };
+        }
+        if (locationValue) {
+            query.locationValue = locationValue;
+        }
+
+        if (startDate && endDate) {
+            query.NOT = {
+                reservations : {
+                    some: {
+                        OR: [
+                            {
+                                endDate: { gte: startDate },
+                                startDate: { lte: startDate }
+                            },
+                            {
+                                startDate: { lte: endDate },
+                                endDate: { gte: startDate }
+                            }
+                        ]
+                    }
+                }
+            }
+        }
+
 
         
         const listings = await prisma.listing.findMany({
